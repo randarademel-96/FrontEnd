52dd3ec75edf3d42d53f2cce9591a6e71acdd0f0
Randara De Mel <rmgdemel@gmail.com>
2025-02-08 19:14:18 +0530

    minor

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -22,7 +22,7 @@ function ProductCard(props) {
 
   return (
     <Card>
-      {count}
+      
       <div className="h-80 bg-card rounded-lg p-4 relative">
         <img src={props.image} className="block" />
       </div>
